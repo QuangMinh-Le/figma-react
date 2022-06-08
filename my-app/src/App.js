2b7a774add04bde12888aa2f6a016bf39c7cb3bc
Lê Quang Minh <106050463+minhlqBHsoft@users.noreply.github.com>
2022-06-08 15:36:36 +0700

    First commit

---
+++ my-app/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Home1 from "./Pages/Home1";
+import Home2 from "./Pages/Home2";
+
+const App = () => (
+    <div>
+        <Home2/>
+    </div>
+);
+
+export default App;
---
+++ my-app/src/Pages/Home2.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Header from "../components/header/header";
+
+
+const Home2 = () => (
+    <div>
+        <Header/>
+    </div>
+);
+ export default Home2;
